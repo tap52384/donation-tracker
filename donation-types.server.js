31b28ec748899ec049018357ce89223675c0f831
Z. Patrick Lewis <patrick_lewis@unc.edu>
2018-05-15 01:58:58 -0400

    explain how to edit code locally

--- donation-types.server.js
+++ donation-types.server.js
@@ -1,11 +1,11 @@
-function fillDonationTypesPicker(showTrashed) {
+function fillDonationTypesPicker( showTrashed ) {
   var filename = 'donation-types';
-  var headers = getHeaders(filename);
-  var nameColIndex = getColumnIndex(headers, 'name');
-  
+  var headers = getHeaders( filename );
+  var nameColIndex = getColumnIndex( headers, 'name' );
+
   var sortArray = [
-      {column: nameColIndex, ascending: true}
+      { column: nameColIndex, ascending: true }
   ];
-  
-  return fillPicker(filename, sortArray, showTrashed);
+
+  return fillPicker( filename, sortArray, showTrashed );
 }
